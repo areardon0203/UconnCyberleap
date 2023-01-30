408ced9c593b0c1c5eee68f6b4a3f87f8da7a0a6
areardon0203 <42280901+areardon0203@users.noreply.github.com>
2023-01-30 18:11:04 -0500

    finished homework

--- Week 2/TestVector.py
+++ Week 2/TestVector.py
@@ -1,6 +1,10 @@
 import unittest
 import math
-from Vector import RecVec, PolVec
+from Vector import Vector, RecVec, PolVec
+
+class testVector(unittest.TestCase):
+    def test_init(self):
+        Vector(5,3,543,6)
 
 class TestRecVec(unittest.TestCase):
     """Test the class RecVec in the file Vector.py"""   
@@ -82,8 +86,74 @@ class TestPolVec(unittest.TestCase):
     def test_init(self):
         """Tests that a Polar Vector object has been created."""
         pv = PolVec(4,30)
-        self.assertEqual(pv.mag, 4)
-        self.assertEqual(pv.ang, 30)
-        print("PolVec INIT Test Passed!")         
+        self.assertEqual(pv.x, 4)
+        self.assertEqual(pv.y, 30)
+        print("PolVec INIT Test Passed!")
+
+    def test_add(self):
+        """Tests that the add function is returning an appropriate tuple"""
+        pv1 = PolVec(3,4)
+        pv2 = PolVec(7,8)
+        self.assertEqual(PolVec.__add__(pv1,pv2), (10,12))
+        print("PolVec Passed add")
+
+    def test_sub(self):
+        """Tests that the add function is returning an appropriate tuple"""
+        pv1 = PolVec(7,8)
+        pv2 = PolVec(3,4)
+        self.assertEqual(PolVec.__sub__(pv1,pv2), (4,4))
+        print("PolVec Passed Subtraction")
+
+    def test_eq(self):
+        """Tests that two tuples are eqivelant"""
+        pv1 = PolVec(3,4)
+        pv2 = PolVec(7-4,4)
+        self.assertEqual(pv1.x,pv2.x)
+        self.assertEqual(pv1.y,pv2.y)
+        print("Passed Equal")
+        
+    def test_str(self):
+        """Tests that we are returning a readable string."""
+        pv = PolVec(3,4)
+        self.assertEqual(str(pv), f"PolVec(x = 3 y = 4)")
+        print("Passed String Test Pol")
+        
+    def test_pol(self):
+        """"Tests that we return the correct polar coordinates from rectangular coordinate input"""
+        pv = PolVec(3,4)
+        PolToRec = pv.pol()
+        self.assertEqual(PolToRec,(5, 53))
+        print("Passed Polar Coordinate test Pol")
+
+    def test_rec(self):
+        """Tests to make sure that the rectangular coordinates are returned in a tuple"""
+        pv = PolVec(5,53)
+        polConvert = PolVec.rectangular(pv)
+        self.assertEqual(polConvert,(3,4))
+        print("Passed Polar to Rec test")
+
+    def test_get_x(self):
+        """Tests that the value of x is returned properly as a single value"""
+        rv = PolVec(3,4)
+        self.assertEqual(rv.get_x(),3)
+        print("Passed PolVec get_x")
+    
+    def test_get_y(self):
+        """Tests that the value of y is returned properly as a single value"""
+        rv = PolVec(3,4)
+        self.assertEqual(rv.get_y(),4)
+        print("Polvec Passed get_y")
+
+    def test_get_mag(self):
+        """Tests to make sure that the objects magnitude is properly returned"""
+        rv = PolVec(3,40)
+        self.assertEqual(rv.mag(),(3))
+        print("Passed get_mag")
+
+    def test_get_ang(self):
+        """Tests that the angle returned is correct"""
+        rv = PolVec(5,40)
+        self.assertEqual(rv.ang(),40)
+        print("Passes get_ang")             
 
 unittest.main()
--- Week 2/Vector.py
+++ Week 2/Vector.py
@@ -1,11 +1,8 @@
 import math
 
-class RecVec:
-    """Creates a class for making rectangle vectors"""
-    def __init__(self,x = 2,y = 2):
-        """Initializes our object with x and y coordinates."""
-        self.x = x
-        self.y = y
+class Vector:
+    def __init__(self, *args):
+        raise NotImplementedError
 
     def __add__(self, other):
         """Adds together two different rectangular vector coordinates."""
@@ -14,6 +11,27 @@ class RecVec:
     def __sub__(self,other):
         """Adds together two different rectangular vector coordinates."""
         return(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        """Checks to make sure two coordinate systems are identical"""
+        return(self.x == other.x, self.y == other.y)
+
+    def pol(self):
+        """Returns a polar vector from rectangular coordinates"""
+        if RecVec:
+            mag = int(math.sqrt(self.x**2 + self.y **2))
+            ang = int(math.degrees(math.atan(self.y/self.x)))
+            return (mag, ang)
+        elif PolVec:
+            return (mag, ang)
+ 
+
+class RecVec(Vector):
+    """Creates a class for making rectangle vectors"""
+    def __init__(self,x = 2,y = 2):
+        """Initializes our object with x and y coordinates."""
+        self.x = x
+        self.y = y
 
     def __str__(self):
         """Converts the default string behavior into a readable output with the proper formatting"""
@@ -45,8 +63,35 @@ class RecVec:
         """Returns the angle from a rectangular vector"""
         return int(math.degrees(math.atan(self.y/self.x)))
 
-class PolVec:
+class PolVec(Vector):
     """Creates a class for making polar vectors"""
     def __init__(self,mag=1, ang=50):
-         self.mag = mag
-         self.ang = ang
+        self.x = mag
+        self.y = ang
+
+    def __str__(self):
+        """Converts the default string behavior into a readable output with the proper formatting"""
+        return f"PolVec(x = {self.x} y = {self.y})"
+
+    def rectangular(self):
+        """First converts y coordinate from degrees to radians and the converts to rectangular coordinates."""
+        dToR = math.radians(self.y)
+        x = round(self.x * math.cos(dToR))
+        y = round(self.x * math.sin(dToR))
+        return (x,y)
+
+    def get_x(self):
+        """Gets the x value from the initialized object"""
+        return self.x
+
+    def get_y(self):
+        """Gets the y value from the initialized object"""
+        return self.y
+
+    def mag(self):
+        """Returns the magnitude based off of the x and y coordinates created in the initialization phase."""
+        return (self.x)
+
+    def ang(self):
+        """Returns the angle from a rectangular vector"""
+        return (self.y)
