d1198000d4ca305a44fd13e51c79d01627a010de
areardon0203 <areardon@crec.org>
2023-02-02 15:56:12 -0500

    fixed time \

--- Week 3/time_functions.py
+++ Week 3/time_functions.py
@@ -1,6 +1,6 @@
 import functions
 import time
-from prettytable import PrettyTable
+
 
 
 
@@ -14,23 +14,23 @@ def time_f(func, n, trials = 10):
         totaltime += time.time() - start
     print(f"Time taken = {totaltime} for n = {n}")
 
-t = PrettyTable('T-Const', 'T-Lin', 'T-Quad' )
+# t = PrettyTable('T-Const', 'T-Lin', 'T-Quad' )
 
 for n in [10, 23, 45, 34, 65, 16, 107, 508, 9]:
     # """this for loop will us the list to run all 3 functions through timetrials"""
-    # print("-"*100) 
-    # print("Function 1: ", end="")
-    # time_f(functions.func1, n)
+    print("-"*100) 
+    print("Function 1: ", end="")
+    time_f(functions.func1, n)
 
-    # print("Function 2: ", end="")
-    # time_f(functions.func2, n)
+    print("Function 2: ", end="")
+    time_f(functions.func2, n)
 
-    # print("Function 3: ", end="")
-    # time_f(functions.func3, n)
+    print("Function 3: ", end="")
+    time_f(functions.func3, n)
 
-    # print("-"*100)            
+    print("-"*100)            
 
 
-    t.add_row([n, time_f(functions.func1), 95])
-    t.add_row([n, time_f(functions.func2), 89])
-    t.add_row([n, time_f(functions.func3), 92])
+    # t.add_row([n, time_f(functions.func1), 95])
+    # t.add_row([n, time_f(functions.func2), 89])
+    # t.add_row([n, time_f(functions.func3), 92])
