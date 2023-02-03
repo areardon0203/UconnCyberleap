190d10e905a2942a2383c11868f2ec25d2314b35
areardon0203 <areardon@crec.org>
2023-02-03 12:54:41 -0500

    almost finished

--- Week 3/time_functions.py
+++ Week 3/time_functions.py
@@ -13,30 +13,33 @@ def time_f(func, n, trials = 10):
         totaltime += time.time() - start
         sortTime.append(totaltime)
     lowestTime = sorted(sortTime)
-    print(n, "  ",lowestTime[0])
+    return lowestTime[0]
 
 """Table for running tests on custom functions, O(1), O(n), O(n^2)"""
 
 print("-"*90) 
-print("N    ", "t_const (ms)              ", "t_lin (ms)              ", "t_quad (ms)")
+print("N",(" "*12), "t_const (ms)",(" "*13), "t_lin (ms)",(" "*18), "t_quad (ms)")
 print("-"*90) 
+
 for n in [10, 23, 45, 34, 65, 16, 10, 58, 90]:
     """this for loop will us the list to run all 3 functions through timetrials"""
-    time_f(functions.func1, n)  
-    time_f(functions.func2, n)
-    time_f(functions.func3, n) 
+    x = time_f(functions.func1, n)
+    y = time_f(functions.func2, n)
+    z = time_f(functions.func3, n)
+    print(f"{n:<10}     {x:.20f}     {y:.20f}     {z:.20f}")
 print("-"*90) 
 
 
 """Python built in atomic behaviors"""
 print("-"*90) 
-print("N    ", "t_list (ms)              ", "t_tup (ms)              ", "t_str (ms)              ,t_set")
+print("N",(" "*12),"t_list (ms)",(" "*14), "t_tup (ms)",(" "*13), "t_str (ms)", (" "*13),"t_set")
 print("-"*90) 
 for n in (1,2,3,4,5,6,7,8,9,10):
     """this for loop will us the list to run all 3 functions through timetrials"""
-    time_f(functions.not_in_list, n)
-    time_f(functions.in_list, n)
-
+    x = time_f(functions.not_in_list, n)
+    y = time_f(functions.in_list, n)
+    
+    print(f"{n:<10}     {x:.20f}     {y:.20f}     ")
     # time_f(functions.func2, n)
     # time_f(functions.func3, n) 
     # print("Function 1: ", end="")
