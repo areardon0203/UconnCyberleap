2041636a06a72677b1ffd44091f3fea8cb380b27
areardon0203 <areardon@crec.org>
2023-02-10 08:43:47 -0500

    stack O(1)

--- Week 4/practice.py
+++ Week 4/practice.py
@@ -11,6 +11,15 @@ class Stack:
     def pop(self):
         return self._L.pop()
 
+    def __len__(self):
+        return len(self._L)
+
+    def peek(self):
+        return self._L[-1]
+
+    def is_empty(self):
+        return len(self._L) == 0
+
 
 if __name__ == "__main__":
 
@@ -24,4 +33,13 @@ if __name__ == "__main__":
     for i in range(n):
         assert s.pop() == n - 1 - i
 
+    assert s.is_empty()
+
+    for i in range(n):
+        assert len(s) == i
+        s.push(i)
+        assert not s.is_empty()
+
+    assert len(s) == n
+    
     print("all done!")
