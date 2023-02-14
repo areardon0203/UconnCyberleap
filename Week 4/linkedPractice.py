21b5c8717d4f9b7758b46cdfea9d1c9e2f68c68e
areardon0203 <areardon@crec.org>
2023-02-14 08:51:49 -0500

    Linked List template

---
+++ Week 4/linkedPractice.py
@@ -0,0 +1,9 @@
+class Node():
+    def __init__(self, data, _head, _next = None):
+        data = self.data
+        _next = self._next
+
+class LinkedList():
+    def __init__(self, data, _next):
+        new_node = Node()
+
