79e1f73d0ccd259edcfa7c77d641f224c509858b
areardon0203 <areardon@crec.org>
2023-02-14 12:49:15 -0500

    linked list string type

--- Week 4/linkedPractice.py
+++ Week 4/linkedPractice.py
@@ -1,7 +1,11 @@
 class Node():
     def __init__(self, data, _head, _next = None):
         data = self.data
+        _head = self._head
         _next = self._next
+
+    def __str__(self, data, _head,_next):
+        pass
 
 class LinkedList():
     def __init__(self, data, _next):
