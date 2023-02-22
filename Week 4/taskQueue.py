62a026e6302fa7334621da7c4d26434aed6d506e
areardon0203 <42280901+areardon0203@users.noreply.github.com>
2023-02-22 09:25:06 -0500

    taskque

--- Week 4/taskQueue.py
+++ Week 4/taskQueue.py
@@ -1,49 +1,89 @@
+class Task:
 
+    def __init__(self, _id, _cycles_left, _next = None, _prev = None):
+        self._id = _id
+        self._cycles_left = _cycles_left
+        self._next = _next 
+        self._prev = _prev
+        self._reduce_cycles = 1
 
-'''create a node object'''
-class Node:
-    def __init__(self, _task):
-        self._task = _task #whatever task
-        self._next = None #node or None 
-        self._prev = None #node or None
+class TaskQueue:
 
-    def __str__(self):
-        if self._next is not None:
-            return (f"Node(task = {self._task}, next._task = {self._next._task})")
-        return(f"Node(task = {self._task},  (next = None)")
-
-class DoublyLinkedList():
     def __init__(self):
-        self._head = None
+        self._tail = None
         self._len = 0
 
-    def add_first_task(self, task):
-        new_task = Node(task,_nex = self._head)   # create new node
-        self._head = new_task               # update LL head
+    def add_task(self, _id, _cycles):
+        """add a new node to the beginning of the linked list"""
+        if self._len == 0: 
+            new_node = Task(_id, _cycles)
+            self._tail = new_node
+        else:  
+            new_node = Task(_id, _cycles, self._head)
+            new_node._next = self._head
+            new_node._prev = self._tail
+            self._tail._next = new_node
+        self._head = new_node
         self._len += 1
 
     def __len__(self):
         return self._len
 
-    def remove_last_task(self):
-        #Edge case: LL is empty
-        if len(self) == 0:
-            raise IndexError("Cannot remove from empty linked list")
-        #Edge case: LL has only 1 task
-        if len(self) == 1:
-            #[head] --> None._next
-            task = self._head._task #retrieve task
-            self._head = None       #cut off tail( tail is head)
-            self._len -= 1          #update length
-            return task             #return task
+    def remove_task(self):
+        # Edge case LL is empty 
+        # if self.len == 0: raise IndexError("Cannot remove from empty linked list")
+        id = self._id
+        self._prev._next = self._next
+        self._next._prev = self._prev
+        self._prev = None
+        self._head = self._next
+        self._len -= 1
+        return id
 
-        #"Typical" case: LL has 2+ tasks
-        #find penultimate of linked list
-        penult = self._head
-        while penult._next._next is not None:
-            penult = penult._next
+    def is_empty(self):
+        if self._len == 0: raise RuntimeError("this task queue is empty")
 
-        task = penult._next._task       #retrieve data
-        penult._next = None             # cut off tail
-        self._len -= 1                  #update length
-        return task                     #return task
+    def execute_tasks(self):
+        cur = self._head
+        while cur:
+            if cur._cycles_left > 0:
+                cur._cycles_left -= cur._reduce_cycles
+                print(f"{cur._id} has {cur._cycles_left} remaining")
+                cur = cur._next
+            else:
+                TaskQueue.remove_task(cur)
+
+    # def add_first(self, _data):
+    #     """add a new node to the beginning of the linked list"""
+    #     if self.len == 0: 
+    #         new_node = Task(_data, None, None)
+    #         self._tail = new_node
+    #     else:  
+    #         new_node = Task(_data, self._head, None)
+    #         self._head._prev = new_node
+    #     self._head = new_node
+    #     self.len += 1
+
+    # def add_last(self, _data):
+    #     """Sets node as head if linked list is empty"""
+    #     if self.len == 0: return self.add_first(_data)
+
+    #     """Searches for the end of the linked list and then inserts new at the end."""
+
+    #     self._tail._next = Task(_data , None, self._tail) #set next node to none and previous node to previous tail 
+    #     self._tail = self._tail._next
+    #     self.len += 1
+
+
+
+    # def remove_last(self):
+    #     # Edge case LL is empty 
+    #     if self.len == 0: raise IndexError("Cannot remove from empty linked list")
+       
+    #     #Edge case: LL has only 1 item
+    #     data = self._tail._data
+    #     self._tail._next = None
+    #     self._tail = self._tail._prev
+    #     self.len -= 1
+    #     return data
+
--- Week 4/testTaskQueue.py
+++ Week 4/testTaskQueue.py
@@ -1,10 +1,61 @@
 from taskQueue import *
+import unittest 
 
 
-dll = DoublyLinkedList()
-n = 10
+tq = TaskQueue()
+tq.add_task(8,25)
+tq.add_task(5,10)
+tq.add_task(7,20)
+tq.execute_tasks()
 
-for task in range(n): dll.add_first_task(task)
-for task in range(n): assert dll.remove_last_task() == task
+task = Task(5,20,30)   
 
-print("add first/last works!")
+
+
+# class MyTestCaseTask(unittest.TestCase):
+    
+    
+ 
+#     def test_init(self):
+   
+#         assert task._id == 5
+#         assert task._cycles_left == 20
+#         assert task._reduce_cycles == 1
+#         print("Task was created properly")
+
+# class MyTestCaseTaskqueue(unittest.TestCase):
+
+#     def test_add_first(self):
+#         assert tq._head._id == 7
+#         assert tq._head._next._id == 5
+#         print("Passed head")
+
+#     def test_len(self):
+#         assert len(tq) == 2
+#         print("length test passed")
+
+#     def test_remove_task(self):
+#         pass
+    
+#     def test_execute_tasks(self):
+#         pass
+
+#     # def test_is_empty(self):
+#     #     with self.assertRaises(RuntimeError): 
+#     #         tq.is_empty()
+#     #         print("Runtime Error Passed")
+
+
+        
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+
+# # for task in range(n): dll.add_first(task)
+# # for task in range(n): assert dll.remove_last() == task
+
+# # print("add first/last works!")
