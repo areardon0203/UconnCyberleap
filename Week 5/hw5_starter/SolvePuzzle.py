0d027a7921975eb89d5fe55019c2721f01a4905d
areardon0203 <areardon@crec.org>
2023-02-27 13:48:26 -0500

    Assert Statements

--- Week 5/hw5_starter/SolvePuzzle.py
+++ Week 5/hw5_starter/SolvePuzzle.py
@@ -1,24 +1,34 @@
-gameboards = [3,1,2,5,2]
 
-def solve_puzzle(): # Make sure to add input parameters here
+def solve_puzzle(gameboard): # Make sure to add input parameters here
     """Returns True(False) if a given board is (is not) solveable"""
 
     # 1) Base case: have you found a valid solution?
 
-    length = len(gameboards)
-    for i in gameboards:
-        if i == length:
-            return True
-        elif i > length:
-            return False
-    # 2) Find all valid next-steps
+    length = len(gameboard) - 1
 
-        elif:
-            pass
+    for i in gameboard:
+        if i == length: return True
+
+    # 2) Find all valid next-steps:
+
+        # elif:
+        #     pass
 
     # 3) Return True if any valid solution is found
 
         else:
-            pass
+            return True
 
-        
+if __name__ == '__main__':
+
+    x = solve_puzzle([2, 1, 4])
+    assert x == True
+
+    y = solve_puzzle([3, 1, 4])
+    assert y == False
+
+    z = solve_puzzle([2,2,1,4])
+    assert z == True
+
+    l = solve_puzzle([1,2,2])
+    assert l == True
