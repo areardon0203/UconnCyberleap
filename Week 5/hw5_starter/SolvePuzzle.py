d6077d7b323b09eae71016a9b3b611674fc01dd4
areardon0203 <areardon0203@gmail.com>
2023-02-27 10:03:29 -0500

    hw5 starter

---
+++ Week 5/hw5_starter/SolvePuzzle.py
@@ -0,0 +1,8 @@
+def solve_puzzle(): # Make sure to add input parameters here
+    """Returns True(False) if a given board is (is not) solveable"""
+
+    # 1) Base case: have you found a valid solution?
+
+    # 2) Find all valid next-steps
+
+    # 3) Return True if any valid solution is found
