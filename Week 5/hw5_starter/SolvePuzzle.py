be08b75b392f80b441c814ef98e97a16dac27f48
areardon0203 <areardon0203@gmail.com>
2023-03-01 11:24:10 -0500

    solvePuzzleTestSchool

--- Week 5/hw5_starter/SolvePuzzle.py
+++ Week 5/hw5_starter/SolvePuzzle.py
@@ -1,13 +1,12 @@
 
-def solve_puzzle(gameboard): # Make sure to add input parameters here
+def solve_puzzle(gameboard, idx = 0): # Make sure to add input parameters here
     """Returns True(False) if a given board is (is not) solveable"""
 
     # 1) Base case: have you found a valid solution?
+    if i == len(gameboard): return True
 
-    length = len(gameboard) - 1
-
-    for i in gameboard:
-        if i == length: return True
+    
+        
 
     # 2) Find all valid next-steps:
 
@@ -16,19 +15,17 @@ def solve_puzzle(gameboard): # Make sure to add input parameters here
 
     # 3) Return True if any valid solution is found
 
-        else:
-            return True
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    x = solve_puzzle([2, 1, 4])
-    assert x == True
+#     x = solve_puzzle([2, 1, 4])
+#     assert x == True
 
-    y = solve_puzzle([3, 1, 4])
-    assert y == False
+#     y = solve_puzzle([3, 1, 4])
+#     assert y == False
 
-    z = solve_puzzle([2,2,1,4])
-    assert z == True
+#     z = solve_puzzle([2,2,1,4])
+#     assert z == True
 
-    l = solve_puzzle([1,2,2])
-    assert l == True
+#     l = solve_puzzle([1,2,2])
+#     assert l == True
--- Week 5/hw5_starter/TestSolvePuzzle.py
+++ Week 5/hw5_starter/TestSolvePuzzle.py
@@ -4,14 +4,31 @@ import unittest
 class TestSolvePuzzle(unittest.TestCase):
         def testClockwise(self):
                 """Tests a board solveable using only CW moves"""
+                
+                x = solve_puzzle([2, 1, 4])
+                assert x == True
 
         def testCounterClockwise(self):
                 """Tests a board solveable using only CCW moves"""
-
+                
+                y = solve_puzzle([3, 1, 4])
+                assert y == False
         def testMixed(self):
                 """Tests a board solveable using only a combination of CW and CCW moves"""
-        
+
+                z = solve_puzzle([2,2,1,4])
+                assert z == True
+
         def testUnsolveable(self):
                 """Tests an unsolveable board"""
 
+                l = solve_puzzle([1,2,2])
+                assert l == True
 unittest.main()
+
+
+
+
+
+
+
