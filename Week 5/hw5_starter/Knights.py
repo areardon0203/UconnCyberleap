a18345ba52c054ac4227dd80744ae69e31728aa3
areardon0203 <42280901+areardon0203@users.noreply.github.com>
2023-03-14 16:32:55 -0400

    homework 5 completed

--- Week 5/hw5_starter/Knights.py
+++ Week 5/hw5_starter/Knights.py
@@ -1,44 +1,34 @@
 def solveable(p_idxs, k_idx):
     """Returns True (false) if all pawn locations can be capture by sequential knight moves"""
     # 1) Base case - is the puzzle solved?
-    if len(p_idx) == 0: return True
+    if len(p_idxs) == 0: return True
 
     # 2) Find all valid_moves
-    valid_moves = knight_moves(k_idx)
+    valid_moves_list = valid_moves(k_idx)
 
     # 3) Try all valid_moves
-
-    for row, col in idx_mod:
-        if valid_moves(k_idx[0]+row,k_idx[1]+col):
-            valid_set.add(row,col)
-
-
+    for pawn in p_idxs:
+        if pawn in valid_moves_list:
+            p_idxs.remove(pawn)
+            k_idx_new = pawn
+            if solveable(p_idxs,k_idx_new):
+                return True
     return False
-
+            
     # 4) If nothing worked in step 3, there's no solution with the knight in this position
 
 
-def knight_moves(k_idx):
+def valid_moves(k_idx):
     """Returns set of all valid moves from k_idx, assuming an 8x8 chess board""" 
+    valid_set = set()
 
     idx_mod = {(2,1),(2,-1),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)}
-    return idx_mod
-
-
-
-        
-def valid_moves(x,y):
-    """Returns set of all valid moves from k_idx, assuming an 8x8 chess board"""
-    return not (x < 0 or y < 0 or x >= 8 or y >= 8)
+    for row, col in idx_mod:
+        if board_check(k_idx[0]+row,k_idx[1]+col):
+            valid_set.add((k_idx[0]+row,k_idx[1]+col))
     
-
-
-
-
-
-
-
-
-k_idx = (0,0)
-knight_moves(k_idx)
-
+    return valid_set
+        
+def board_check(x,y):
+    """Returns set of all valid moves from k_idx, assuming an 8x8 chess board"""
+    return not (x < 0 or y < 0 or x > 7 or y > 7)
--- Week 5/hw5_starter/SolvePuzzle.py
+++ Week 5/hw5_starter/SolvePuzzle.py
@@ -1,29 +1,45 @@
-def memo_solve_puzzle(board, idx=0):
-    solved ={}
+def memo_solve_puzzle(board, idx=0, solved=None):
+    """This function stores a cache of solved results"""
+    if solved is None:
+        solved ={}
     return _solve_puzzle(board, idx, solved)
 
 
 def _solve_puzzle(board, idx, solved):
-    if idx in solved: return solved[idx]
+    """This function checks if the puzzle is solveable or not"""
+    #base case
+    if idx in solved: return solved[idx] 
     if idx == len(board) - 1: return True
 
-    solved[idx] = idx
+    solved[idx] = False
 
+    #stores values of clockwise and counter clock movement from current space
     idx_cw = (idx + board[idx]) % len(board)
     idx_ccw = (idx - board[idx]) % len(board)
+    
+    # n = False
 
-    n = False
-
+    
     for move in {idx_cw, idx_ccw}:
-        
+        if move not in solved and _solve_puzzle(board,move,solved):
+            solved[idx] = True
+            return True
+               
+    return solved[idx]
+    
+
         # if current_optimum == True:
         #     return solved[idx] == True
-        if move not in solved:
-            n = _solve_puzzle(board, move, solved)
-        
-            if n is not False:   
-                solved[idx] = n
-                return solved[idx]
-            
-    return solved[idx]
 
+
+
+
+    if idx_cw not in solved:
+        if _solve_puzzle(board, idx_cw, solved):
+            solved[idx] = True
+            return True
+            
+    if idx_ccw not in solved:
+        if _solve_puzzle(board, idx_ccw, solved):
+            solved[idx] = True
+            return True
